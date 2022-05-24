4c7ba2b4fbf21b11804d268d94968b3daa73e4b1
mysilvercloud <106033894+mysilvercloud@users.noreply.github.com>
2022-05-24 15:07:52 +0200

    Add files via upload

--- password.py
+++ password.py
@@ -3,8 +3,9 @@ print('HElLO, THIS IS MY RANDOM PASSWORD GENERATOR, PLEASE INPUT YOUR PASSWORD L
 import random
 # create input for user 
 length = int(input('\nEnter the length of password: '))
-# create variable from module RANDOM, the maxium length of the password depends on your computer
-temp = random.sample(all,length)
+# create variable from module RANDOM, the maxium length of the password is 36
+x = ("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
+temp = random.sample(sorted(x),length)
 # join the temp to password
 password = "".join(temp)
 # execution
